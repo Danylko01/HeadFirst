e049cef0c74c651e6cb62478d6d2d604e7836f79
Danylko <guanghua_dan@163.com>
2024-02-27 05:48:49 +0800

    修改工厂方法

--- src/factoryMethod.cpp
+++ src/factoryMethod.cpp
@@ -1,55 +1,86 @@
-#include<iostream>
-
+#include <iostream>
+#include <string>
 using namespace std;
 
-class Transport {
+// 抽象产品类
+class Product {
 	public:
-		virtual ~Transport(){}
-		virtual void deliver() const = 0;
-};
-class Truck:public Transport {
-	public:
-		void deliver() const override{
-			cout << "卡车运输货物中ing\n";
+		virtual ~Product(){}
+		virtual void display() const = 0;
+		void productMethod() {
+			cout << "parent method" << endl;
 		}
 };
-
-class Ship:public Transport{
-	public: 
-		void deliver() const override{
-			cout << "轮船运输货物中ing \n";
+// 具体产品类A
+class ConcreteProductA : public Product {
+	private:
+		string propertyA;
+	public:
+		ConcreteProductA(const string& propA) : propertyA(propA){}
+		void display() const override {
+			cout << "ConcreteProductA with property:" << propertyA << endl;
+		}
+		void additionMethodA() const {
+			cout << "Additional method specific to ConcreteProductA" << endl;
 		}
 };
-class Logistics {
+// 具体产品类B
+class ConcreteProductB : public Product {
+	private:
+		int propertyB;
 	public:
-		virtual ~Logistics(){}
-		virtual Transport* factoryMethod() const = 0;
-		void doSomething() {
-			Transport* transport = factoryMethod();
-			transport->deliver();
-			delete transport;
+		ConcreteProductB(int propB) : propertyB(propB) {}
+		void display() const override {
+			cout << "ConcreteProductB with property:" << propertyB << endl;
+		}
+		void additionMethodB() const {
+			cout << "Additional method specific to ConcreteProductB" << endl;
 		}
 };
-class TruckLogistics: public Logistics {
+// 抽象工厂类
+class Factory {
 	public:
-		Transport* factoryMethod() const override {
-			return new Truck();
+		virtual ~Factory(){}
+		virtual Product* createProduct() const = 0;
+};
+// 具体工厂类A
+class ConcreteFactoryA : public Factory {
+	public:
+		Product* createProduct() const override {
+			return new ConcreteProductA("propertyA_value");
 		}
 };
-class ShipLogistics: public Logistics {
+// 具体工厂类B
+class ConcreteFactoryB : public Factory {
 	public:
-		Transport* factoryMethod() const override {
-			return new Ship();
+		Product* createProduct() const override {
+			return new ConcreteProductB(24);
 		}
 };
 int main() {
-	Logistics* truckLogistics = new TruckLogistics();
-	Logistics* shipLogistics = new ShipLogistics();
-	truckLogistics->doSomething();
-	truckLogistics->doSomething();
-	shipLogistics->doSomething();
-	shipLogistics->doSomething();
-	delete truckLogistics;
-	delete shipLogistics;
+	// 使用具体工厂A创建产品A
+	Factory* factoryA = new ConcreteFactoryA();
+	Product* productA = factoryA->createProduct();
+	productA->display();
+	productA->productMethod();
+	// 调用特有方法和属性
+	ConcreteProductA* concreteProductA = dynamic_cast<ConcreteProductA*>(productA);
+	if (concreteProductA) {
+		concreteProductA->additionMethodA();
+	}
+	// 使用具体工厂B创建产品B
+	Factory* factoryB = new ConcreteFactoryB();
+	Product* productB = factoryB->createProduct();
+	productB->display();
+	// 调用特有的方法和属性
+	ConcreteProductB* concreteProductB = dynamic_cast<ConcreteProductB*>(productB);
+	if (concreteProductB) {
+		concreteProductB->additionMethodB();
+	}
+	// 释放资源
+	delete factoryA;
+	delete productA;
+	delete factoryB;
+	delete productB;
 	return 0;
 }
